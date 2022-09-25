bdf7c7fe2864173a450e3607e7265373922f570e
Matyi01 <holczermatyi@gmail.com>
2022-09-25 20:18:01 +0200

    hf3

---
+++ hf1/hf3.py
@@ -0,0 +1,14 @@
+print("hf3.1:")
+szam = 2
+while szam < 101:
+    print(szam)
+    szam += 2
+print("hf3.2:")
+lista1=["a","b","c"]
+lista1.reverse()
+print(lista1)
+print("hf3.3:")
+lista2=["t",str(4),"f",str(1),"z",str(7)]
+lista2.sort()
+for x in lista2:
+    print(x)
