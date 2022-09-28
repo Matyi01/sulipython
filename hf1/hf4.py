50814d2d082f831161505df6b1033ab66b06d43f
Matyi01 <holczermatyi@gmail.com>
2022-09-28 19:55:56 +0200

    hf4

---
+++ hf1/hf4.py
@@ -0,0 +1,4 @@
+lorem="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis posuere elit. Vivamus dictum libero ut iaculis viverra. Suspendisse accumsan porttitor quam quis maximus. Duis a arcu vel justo placerat fermentum. Aenean convallis volutpat eros. Phasellus turpis magna, vestibulum sed tellus eget, accumsan fringilla leo. Ut nec dui vitae risus semper suscipit. Praesent eu bibendum sem. Cras feugiat quis erat ut porta."
+print(lorem,len(lorem))
+print(lorem.replace(" ",""),len(lorem.replace(" ","")))
+print(lorem.replace("est",""),len(lorem.replace("est","")))
