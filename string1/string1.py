4717f5a3e42e5acf4e8c1ca8f302ead8b941b753
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2022-10-10 11:54:11 +0200

    string1

---
+++ string1/string1.py
@@ -0,0 +1,17 @@
+nev = input("Kérek egy nevet: ")
+print("A "+nev+" nevet írtad be.")
+print("A {belsonev} nevet írtad be.".format(belsonev = nev))
+if len(nev) < 5:
+    print("Jó rövid név.")
+elif len(nev) >= 10:
+    print("Very big név.")
+be = "nemvégjel"
+szavak=[]
+while be != "":
+    be = input("Írj be valamit: ")
+    szavak.append(be)
+#szavak.remove("")
+#szavak.pop(-1)
+#szavak.pop(len(szavak)-1)
+#szavak = szavak[:-1]
+print(szavak)
