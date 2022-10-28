fedf99c64bcc4d15724a7ea9d7274a7acde4e98e
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2022-10-28 08:44:47 +0200

    masodfok2

--- valtozok/masodfok.py
+++ valtozok/masodfok.py
@@ -3,5 +3,13 @@ import math
 a = int(input("a="))
 b = int(input("b="))
 c = int(input("c="))
-print("x1="(-b-math.sqrt(b**2-4*a*c))/2*a)
-print("x2="(-b+math.sqrt(b**2-4*a*c))/2*a)
+diszkriminans = b*b-4*a*c
+if diszkriminans < 0:
+    print("Nincs megoldás")
+elif diszkriminans == 0:
+    megoldas = -b/(2*a)
+    print("Egy megoldás {}".format(megoldas))
+else:
+    x1 = (-b-math.sqrt(diszkriminans))/(2*a))
+    x2 = (-b+math.sqrt(diszkriminans))/(2*a))
+    
