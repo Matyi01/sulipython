015c5a49dc50404f4375b55e1194c6cfc125d929
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2022-11-04 10:34:29 +0100

    matplotlib2

--- matplotlin1/matplotlib1.py
+++ matplotlin1/matplotlib1.py
@@ -1,8 +1,16 @@
+#pip install matplotlib --proxy http://tanulo01:Tanulo0118@172.16.0.5:8080
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
+xpoints = np.array([0, 20])
+ypoints = np.array([0, 20])
 
 plt.plot(xpoints, ypoints)
+plt.plot([0,0],[10,0])
+plt.plot([0,10],[0,0])
+plt.plot([10,10],[0,10])
+plt.plot([0,10],[10,10])
+plt.plot([0,5],[10,17.5])
+plt.plot([5,10],[17.5,10])
 plt.show()
