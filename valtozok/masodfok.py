ecd04601a6a3e40ae4ecb57040c66aac56d6d702
Matyi01 <holczermatyi@gmail.com>
2022-11-06 11:00:53 +0100

    masodfokhf

--- valtozok/masodfok.py
+++ valtozok/masodfok.py
@@ -1,8 +1,33 @@
 import math
 #a*x2+b*x+c
-a = int(input("a="))
-b = int(input("b="))
-c = int(input("c="))
+
+a = ""
+while a == "":
+    try:
+        a = int(input("a = "))
+    except ValueError:
+        print("Ez nem szám!")
+    except:
+        print("Ismeretlen hiba!")
+
+b = ""
+while b == "":
+    try:
+        b = int(input("b = "))
+    except ValueError:
+        print("Ez nem szám!")
+    except:
+        print("Ismeretlen hiba!")
+
+c = ""
+while c == "":
+    try:
+        c = int(input("c = "))
+    except ValueError:
+        print("Ez nem szám!")
+    except:
+        print("Ismeretlen hiba!")
+
 diszkriminans = b*b-4*a*c
 if diszkriminans < 0:
     print("Nincs megoldás")
@@ -10,6 +35,6 @@ elif diszkriminans == 0:
     megoldas = -b/(2*a)
     print("Egy megoldás {}".format(megoldas))
 else:
-    x1 = (-b-math.sqrt(diszkriminans))/(2*a))
-    x2 = (-b+math.sqrt(diszkriminans))/(2*a))
+    x1 = (-b-math.sqrt(diszkriminans))/(2*a)
+    x2 = (-b+math.sqrt(diszkriminans))/(2*a)
     
