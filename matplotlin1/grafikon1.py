f4c350b74993049c64d95c4a418e23993e5bb15d
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2022-11-07 13:10:36 +0100

    grafikon1

---
+++ matplotlin1/grafikon1.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+#x = [1,10]
+#y = [10,1]
+#plt.plot(x,y)
+#plt.show()
+
+#y = 6x^2 + 4x - 16
+
+xPont = []
+yPont = []
+for x in range(-10,10):
+    xPont.append(x)
+    yPont.append(6*x**2 + 4*x - 16)
+
+plt.plot(xPont,yPont)
+
+#y = 16x^2 - 3 + 16
+
+xPont = []
+yPont = []
+for x in range(-10,10):
+    xPont.append(x)
+    yPont.append(16*x**2 - 3*x + 16)
+
+plt.plot(xPont,yPont)
+
+plt.show()
