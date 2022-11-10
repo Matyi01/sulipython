37f09f063dc7fe999b4e43cf348c591b28b8b7f2
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2022-11-10 13:19:57 +0100

    masodfok2

--- valtozok/masodfok.py
+++ valtozok/masodfok.py
@@ -1,6 +1,22 @@
 import math
 #a*x2+b*x+c
 
+def egyenlet(a,b,c):
+    szoveg = "0 = "
+    if a != 0:
+        szoveg += str(a) + "x²"
+    if b > 0:
+        szoveg += " + " + str(b) + "x"
+    elif b < 0:
+        szoveg += " " + str(b) + "x"
+    if c > 0:
+        szoveg += " + " + str(c)
+    elif c < 0:
+        szoveg += " " + str(c)
+
+    return szoveg
+
+
 a = ""
 while a == "":
     try:
@@ -37,4 +53,6 @@ elif diszkriminans == 0:
 else:
     x1 = (-b-math.sqrt(diszkriminans))/(2*a)
     x2 = (-b+math.sqrt(diszkriminans))/(2*a)
+    print("A megoldás x1:[], x2:[]".format(x1,x2))
     
+print(egyenlet(a,b,c))
