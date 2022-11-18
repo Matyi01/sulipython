479c24364be35d7057f1639870d5bbb4837cace0
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2022-11-18 14:04:41 +0100

    szambe2

--- valtozok/SzamBe.py
+++ valtozok/SzamBe.py
@@ -3,8 +3,22 @@
 #2022. 11. 18. Holczer Mátyás
 
 def szamBe(szoveg,tort=False,minimum=False,maximum=False):
-    print(szoveg)
-    print(tort)
+#    print(szoveg)
+#    print(tort)
+#    print(minimum)
 
-szamBe("Aggyá meg számót: ",tort=True)
+    hiba = True
+    while hiba:
+        try:
+            if tort:
+                szam = float(input(szoveg))
+            else:
+                szam = int(input(szoveg))
+
+        except:
+            print("Hiba!")
+        else:
+            hiba = False
+    
+szamBe("Aggyá meg számót: ",minimum=10,tort=True)
 
