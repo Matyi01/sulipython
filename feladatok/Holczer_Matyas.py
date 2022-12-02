b66e58a5bd368b9a33a1c07ae3481f952cb77c5f
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2022-12-02 10:33:07 +0100

    gyak1202

---
+++ feladatok/Holczer_Matyas.py
@@ -0,0 +1,56 @@
+szamok=[]
+for i in range(0,8):
+    szamok.append(int(input("Adj meg egy srámot: ")))
+print(szamok)
+#for i ion szamok:
+#   print(str(i),end="")
+#print()
+print(szamok[0:4])
+print(szamok[4:8])
+#for i in range(8):
+#   print(szamok(i)+"\t",end="")
+#   if i%4 == 0:
+#       print()
+#print()
+osszeg = szamok[0] + szamok[1] + szamok[2] + szamok[3] + szamok[4] + szamok[5] + szamok[6] + szamok[7]
+print(osszeg)
+#osszeg = 0
+#print(sum(szamok))
+#for i in szamok:
+#   osszeg += i
+#print(osszeg)
+szoveg = "Nulla quis mi augue. Nunc vel pretium lectus. Aenean laoreet ornare ornare. Ut vitae elit et sapien fringilla iaculis ac at felis. Aenean scelerisque, diam non pellentesque rhoncus, risus lorem porttitor leo, ac consectetur nisi massa vitae sem. Nulla tempus diam id bibendum lobortis. Vestibulum porta neque id risus cursus, eget sodales nunc fermentum. Nulla facilisi. Suspendisse egestas orci a luctus fringilla. Cras dapibus ipsum nisl, non dapibus ex fermentum vitae."
+#jelek = ",.!?-:;'"
+#for i in range(len(jelek)):
+#   szoveg = szoveg.reaplace(jelek[i],"")
+#print(szoveg)
+betu = ""
+while betu != "end":
+    betu = input("Adj meg egy betűt: ")
+    print(szoveg.count(betu))
+print(szoveg[::-1])
+szoveg.replace("orna","")
+#szoveg = szoveg.replace("orna","")
+print(len(szoveg))
+
+def fenyo():
+    x = ""
+    while x == "":
+        x = input("Adj meg egy karaktert: ")
+    fenyo = """
+               {x}
+              {x}{x}{x}
+             {x}{x}{x}{x}{x}
+            {x}{x}{x}{x}{x}{x}{x}
+              {x}{x}{x}
+             {x}{x}{x}{x}{x}
+            {x}{x}{x}{x}{x}{x}{x}
+              {x}{x}{x}
+             {x}{x}{x}{x}{x}
+            {x}{x}{x}{x}{x}{x}{x}
+               {x}
+              {x}{x}{x}   
+            """
+    print(fenyo.format(x=x))
+    
+fenyo()
