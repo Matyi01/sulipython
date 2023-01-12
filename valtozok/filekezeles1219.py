77cd847c870a7f09555a204a30cb6f013d175b0d
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-01-12 13:18:45 +0100

    filecucc

--- valtozok/filekezeles1219.py
+++ valtozok/filekezeles1219.py
@@ -4,5 +4,23 @@ f.write("Hello")
 f.write("\n")
 f.write("világ")
 
+f.close()
+
+f = open("proba.txt","r")
+
+szoveg = f.read()
+
+sorok = szoveg.split("\n")
+print(sorok)
+
+f.close()
+
+f = open("proba.txt","r")
+
+sorok = []
+for sor in f:
+    sorok.append(sor.replace("\n","").replace("\r",""))
+
+print(sorok)
 
 f.close()
