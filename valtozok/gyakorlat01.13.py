6aefb85d29d909eaf933a49670e3483837fd65bf
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-01-13 14:00:17 +0100

    gyakorlas01.13_2

--- valtozok/gyakorlat01.13.py
+++ valtozok/gyakorlat01.13.py
@@ -1,10 +1,9 @@
 import random
 
-f = open("datumok.txt","w")
-
-for i in range(100000):
+def randomDatum():
     ev = random.randint(2003,2008)
     h = random.randint(1,12)
+    hk = ["Január","Február","Március","Április","Május","Június","Július","Augusztus","Szeptember","Október","November","December"]
     if h == 1 or h == 3 or h == 5 or h == 7 or h == 8 or h == 10 or h == 12:
         nap = random.randint(1,31)
     elif h == 4 or h == 6 or h == 9 or h == 11:
@@ -14,9 +13,18 @@ for i in range(100000):
             nap = random.randint(1,29)
         else:
             nap = random.randint(1,28)
-    ev = str(ev)
-    h = str(h)
-    nap = str(nap)
-    f.write(ev + ". " + h + ". " + nap + ". ")
+    if nap // 10 == 0:
+        nap = str(nap).zfill(2)
+    if h // 10 == 0:
+        h = str(h).zfill(2)
+    datum = str(ev) + ". " + hk[int(h)-1] + ". " + str(nap) + ". "
+    return datum
 
-f.close()
+def fileIras():
+    f = open("datumok.txt","w")
+    for i in range(10):
+        f.write(randomDatum()+"\n")
+    f.close()
+
+
+fileIras()
