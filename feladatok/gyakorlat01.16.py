25102788e67360a958e396f99e4b0385071bb3d1
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-01-24 11:27:58 +0100

    gyak01.24

--- feladatok/gyakorlat01.16.py
+++ feladatok/gyakorlat01.16.py
@@ -2,18 +2,22 @@ import random
 import datetime
 
 def szoBe():
+#ures sor vegjelig keri be
     x = "a"
     while x != "":
         f = open("szotar.txt","a")
 
+#szavak bekerese
         x = input("kérek egy szót: ")
         if x == "":
             y = ""
         else:
             y = input("kérem a jelentését: ")
 
+#datum generalasa a szoparhoz
         ido = datetime.date.today()
-        
+
+#adatok hozzaadasa a fajlhoz
         if x != "":
             f.write(str(x)+" : "+str(y)+" : "+str(ido)+"\n")
         
@@ -22,57 +26,81 @@ def szoBe():
 def szoRandom():
     f = open("szotar.txt","r")
 
+#fajl adatait listava alakitja amivel lehet dolgozni
     full = f.readlines()
     for i in range(len(full)):
         full[i] = full[i].split(" : ")
         full[i][2] = full[i][2].strip("\n")
 
-    ido = input("mettől szeretnéd a szavakat? (YYYY-MM-DD): ")
+#bekeri hogy milyen ido legyen a legkisebb amitol kerdezi
+    idomin = input("mettől szeretnéd a szavakat? (YYYY-MM-DD): ")
 
+#elso elem keresese a listaban amiben megvan a bekert ido
     for i in range(len(full)):
-        if str(full[i][2]) == str(ido):
-            ido = i
-    print(ido)
+        if str(full[i][2]) == str(idomin):
+            idomin = i
 
+#bekeri hogy milyen ido legyen a lenagyobb ameddig kerdezi
+    idomax = input("meddig szeretnéd a szavakat? (YYYY-MM-DD): ")
+
+#elso elem keresese hatulrol a listaban amiben megvan a bekert ido
+    for i in reversed(range(len(full))):
+        if str(full[i][2]) == str(idomax):
+            idomax = i
+
+#jo es rossz valaszok szamanak valtozoi
+    jo = 0
+    rossz = 0
     
+#ures sor vegjelig kerdez
     b = "a"
     while b != "":
-
-        print(full[4],full[5],full[6],full[7])
-            
-        x = full[random.randint(ido,len(full)-1)]
+#szo generalasa amit kerdez
+        x = full[random.randint(idomin,idomax)]
         szo1 = x[1]
-        
+
+#megkerdezi a generalt szo jelenteset
         print(x[0] + " szó jelentése?")
 
-#while y == x:
-        y = full[random.randint(ido,len(full)-1)]
+#meg harom szavat general amik nem jo valaszok es nem ugyanazon szavak mint az elozoek
+        y = full[random.randint(idomin,idomax)]
+        while y == x:
+            y = full[random.randint(idomin,idomax)]
 
         szo2 = y[1]
 
-        
-        z = full[random.randint(ido,len(full)-1)]
+        z = full[random.randint(idomin,idomax)]
+        while z == x or z == y:
+            z = full[random.randint(idomin,idomax)]
 
         szo3 = z[1]
-
-
-        q = full[random.randint(ido,len(full)-1)]
+        
+        q = full[random.randint(idomin,idomax)]
+        while q == x or q == y or q == z:
+            q = full[random.randint(idomin,idomax)]
         szo4 = q[1]
 
-
-        
+#generalt szavak listaba rakasa es keverese
         szavak = [szo1,szo2,szo3,szo4]
         random.shuffle(szavak)
         print(szavak[0]+", "+szavak[1]+", "+szavak[2]+", "+szavak[3])
-
+        
+#bekeri a szo jelenteset es megnezi hogy jo e
         b = input()
         if b == "":
             break
         if b == x[1]:
             print("Jol tudtad!")
+            jo += 1
         else:
             print("Nem jo...")
+            rossz += 1
 
+#jo es rossz valaszok szamanak kiiratasa
+    print(str(jo)+" jo es "+str(rossz)+" rossz valaszod volt")
+    szazalek = (jo / (jo + rossz)) * 100
+    
+    print("az eredmenyed: "+str(round(szazalek,2))+"%")
     f.close()
 
 
