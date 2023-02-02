355e0c6327adf3f3ab371148522eaff9f684fbf0
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-02-02 12:34:55 +0100

    gyak0202

--- erettsegi_feladatok/Szamok/szamok.py
+++ erettsegi_feladatok/Szamok/szamok.py
@@ -1,3 +1,5 @@
+import random
+
 f = open("felszam.txt","r")
 
 kerdesek = []
@@ -20,5 +22,42 @@ for e in kerdesek:
 
 print("Az adatjaflban "+str(len(matek))+" matematika feladat van, 1 pontot er "+str(matek.count(1))+" feladat, 2 pontot er "+str(matek.count(2))+" feladat, 3 pontot er "+str(matek.count(3))+" feladat. ")
 
-valaszok=[e[1] for e in kerdesek]
+valaszok = [e[1] for e in kerdesek]
 print("A valaszok számértéke {}-től {}-ig tart.".format(min(valaszok),max(valaszok)))
+
+temakorok = []
+for e in kerdesek:
+    if e[3] not in temakorok:
+        temakorok.append(e[3])
+
+print("5. feladat:")
+print("A témakörök: "+", ".join(temakorok))
+
+print("6. feladat:")
+beker = input("Milyen témakörből szeretne kérdést kapni? ")
+ujlista = [e for e in kerdesek if e[3] == beker]
+sorsolt = random.choice(ujlista)
+valasz = input(sorsolt[0])
+if valasz == sorsolt[1]:
+    print("A válasz "+sorsolt[2]+"pontot ér.")
+else:
+    print("A válasz 0 pontot ér.")
+    print("A helyas válasz: "+str(sorsolt[1]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
