1b010cd251c98ec7ab6136f04cf29d033d290ce1
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-02-03 09:39:30 +0100

    gyak02.03

--- erettsegi_feladatok/Szamok/szamok.py
+++ erettsegi_feladatok/Szamok/szamok.py
@@ -38,20 +38,33 @@ beker = input("Milyen témakörből szeretne kérdést kapni? ")
 ujlista = [e for e in kerdesek if e[3] == beker]
 sorsolt = random.choice(ujlista)
 valasz = input(sorsolt[0])
-if valasz == sorsolt[1]:
-    print("A válasz "+sorsolt[2]+"pontot ér.")
+if int(valasz) == sorsolt[1]:
+    print("A válasz "+str(sorsolt[2])+" pontot ér.")
 else:
     print("A válasz 0 pontot ér.")
     print("A helyas válasz: "+str(sorsolt[1]))
 
+print("7. feladat:")
+lista10 = []
+for i in range(10):
+    r = random.choice(kerdesek)
+    while r in lista10:
+        r = random.choice(kerdesek)
+    lista10.append(r)
+print(lista10)
 
+random.shuffle
+lista10 = kerdesek[0:10]
+print(lista10)
 
-
-
-
-
-
-
+f = open("tesztfel.txt","w")
+ossz = 0
+for e in lista10:
+    f.write(str(e[2])+" "+str(e[1])+" "+e[0]+"\n")
+    ossz += e[2]
+f.write("A feladatsorra összesen {} pont adható.".format(ossz))
+    
+f.close()
 
 
 
