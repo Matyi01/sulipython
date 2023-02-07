e3ce03981e7d22630116ea1d8e7e5d9e33f488cc
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-02-07 11:28:38 +0100

    gyak02.07

--- erettsegi_feladatok/Tesztverseny/tesztverseny.py
+++ erettsegi_feladatok/Tesztverseny/tesztverseny.py
@@ -11,6 +11,36 @@ adatok.pop(0)
 valaszok = []
 for e in adatok:
     valaszok.append(e.split(" "))
-
     
-print(valaszok)
+print("2. feladat: A vetélkedőn "+str(len(valaszok))+" versenyző indult.")
+
+versenyzo = input("3. feladat: A versenyző azonosítója = ")
+versenyzovalasza = ""
+for e in valaszok:
+    if e[0] == versenyzo:
+        print(e[1]+"\t (a versenyző válasza)")
+        versenyzovalasza = e[1]
+
+#print("{} \t (a versenyző válasza)".format([e[1] for e in valaszok if e[0] == versenyzo[0]]))
+
+print("4. feladat")
+print(helyes+"\t (a helyes megoldás)")
+print(versenyzovalasza)
+
+for sorszam, betu in enumerate(versenyzovalasza):
+    if betu == helyes[sorszam]:
+        print("+", end = "")
+    else:
+        print(" ", end = "")
+print("\t (a versenyző helyes válaszai)")
+
+
+
+
+
+
+
+
+
+
+
