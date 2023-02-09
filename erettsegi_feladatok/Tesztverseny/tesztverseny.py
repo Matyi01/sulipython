c2484b287e939497d81508116bd1b8e2cf6bfde5
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-02-09 13:20:27 +0100

    0209

--- erettsegi_feladatok/Tesztverseny/tesztverseny.py
+++ erettsegi_feladatok/Tesztverseny/tesztverseny.py
@@ -1,3 +1,14 @@
+def pontszamit(valasz, helyes):
+    pont = 0
+    for sorszam, betu in enumerate(valasz):
+        if betu == helyes[sorszam]:
+            if sorszam < 5:
+                pont += 3
+            elif sorszam < 10:
+                pont += 4
+
+
+
 f = open("valaszok.txt")
 
 adatok = f.read().split("\n")[:-1]
@@ -34,6 +45,22 @@ for sorszam, betu in enumerate(versenyzovalasza):
         print(" ", end = "")
 print("\t (a versenyző helyes válaszai)")
 
+feladat = int(input("5. feladat: A feladat sorszáma = "))
+
+db = 0
+        
+for e in valaszok:
+    if e[1][feladat] == helyes[feladat]:
+        db += 1
+
+print("A feladatra {0} fő, a versenyzők {1:.2%}-a adott helyesválaszt.".format(db, db/len(valaszok)))
+
+f = open("pontok.txt","w")
+for e in valaszok:
+    pont = pontszamit(e[1], helyes)
+
+
+f.close()
 
 
 
