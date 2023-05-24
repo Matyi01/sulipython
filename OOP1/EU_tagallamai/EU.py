e22a8a991c41fdfb364ef089aedbf0fa9c01adcc
Matyi01 <holczer.matyi.21@ady-nagyatad.hu>
2023-05-24 19:45:05 +0200

    EUkesz

--- OOP1/EU_tagallamai/EU.py
+++ OOP1/EU_tagallamai/EU.py
@@ -13,18 +13,49 @@ for e in f:
     adatok.append(eu(*temp))
 f.close()
 
-print("3. feladat")
 csatlakozott = 0
 for e in adatok:
-    if int(e.ev[0:4]) <= 2018:
+    if int(e.ev[:4]) <= 2018:
         csatlakozott += 1
-print(csatlakozott)
+print(f"3. feladat: EU tagállamainak száma: {csatlakozott}")
 
-print("4. feladat")
 csatlakozott2007 = 0
 for e in adatok:
-    if int(e.ev[0:4]) == 2007:
+    if e.ev[:4] == "2007":
         csatlakozott2007 += 1
-print(csatlakozott2007)
+print(f"4. feladat: 2007-ben {csatlakozott2007} ország csatlakozott.")
 
-print("5. feladat")
+for e in adatok:
+    if e.orszag == "Magyarország":
+        print(f"5. feladat: Magyarország csatlakozásának dátuma: {e.ev}")
+
+majus = False
+for e in adatok:
+    if e.ev[6:7] == "5":
+        majus = True
+if majus:
+    print("6. feladat: Májusban volt csatlakozás!")
+
+evek = []
+for e in adatok:
+    evek.append(e.ev[:4])
+legutobbi = ""
+for e in adatok:
+    if max(evek) == e.ev[:4]:
+        legutobbi = e.orszag
+print(f"7. feladat: Legutoljára csatlakozott ország: {legutobbi}")
+
+print("8. feladat: Statisztika")
+stat = {}
+for e in adatok:
+    stat[e.ev[:4]] = 0
+for e in adatok:
+    stat[e.ev[:4]] += 1
+for e in stat:
+    print(f"\t{e} - {stat[e]} ország")
+
+
+
+
+
+
